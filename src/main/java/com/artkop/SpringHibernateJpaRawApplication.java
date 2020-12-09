f78a48e088d0c365e27587bf110d95cb694eecd7
Artem Kopilov <artemykopilov@gmail.com>
2020-12-09 12:00:08 +0300

    initial commit

---
+++ src/main/java/com/artkop/SpringHibernateJpaRawApplication.java
@@ -0,0 +1,13 @@
+package com.artkop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHibernateJpaRawApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHibernateJpaRawApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/artkop/SpringHibernateJpaRawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artkop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHibernateJpaRawApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
