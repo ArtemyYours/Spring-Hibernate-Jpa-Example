5529c7aeaf8d931e18522bb443f744afcad94e83
Artem Kopilov <artemykopilov@gmail.com>
2020-12-09 13:46:59 +0300

    correct model created

--- src/main/java/com/artkop/SpringHibernateJpaRawApplication.java
+++ src/main/java/com/artkop/SpringHibernateJpaRawApplication.java
@@ -2,8 +2,10 @@ package com.artkop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringHibernateJpaRawApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/artkop/model/Person.java
@@ -0,0 +1,20 @@
+package com.artkop.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+}
